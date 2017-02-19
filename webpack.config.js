d91feb355aa675aa481fd0ce4df4aae4d36a54c7
Jaspreet Singh <jaspreet_xps@yahoo.com>
2017-02-19 04:18:30 -0800

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const vendor_libs = [
+  'react', 'react-dom', 'react-dom', 'react-router-dom'
+]
